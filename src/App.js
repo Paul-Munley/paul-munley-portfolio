60ffb73f8469ecaeeb85f1c6e3d43b74466a09af
Paul-Munley <paulrmunley21@gmail.com>
2021-06-18 21:18:19 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import Layout from "./layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      {/* <Switch>
+        <Route path="/">
+          <Homepage />
+        </Route>
+        <Route exact path="*">
+          <p>page not found</p>
+        </Route>
+      </Switch> */}
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/layout/Layout.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import Header from "../components/Header";
+
+const Layout = props => {
+  return (
+    <Fragment>
+      <Header />
+      <div>{props.children}</div>
+    </Fragment>
+  );
+};
+
+export default Layout;
