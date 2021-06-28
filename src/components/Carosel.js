b6f770cd6b5a82a3077c27cbe1783716532118d9
Paul-Munley <paulrmunley21@gmail.com>
2021-06-28 14:25:58 -0700

    Got portfolio carousel set up and working. Ended up using useState for each img over useReducer. Carousel slider seems to be loading slowly with high res images so may need to fix this up later on once I add actual porfolio images.

--- src/components/Carosel.js
+++ src/components/Carosel.js
@@ -1,182 +1,78 @@
 import classes from "./Carosel.module.css";
-import { useReducer, useState } from "react";
+import { Fragment, useEffect, useReducer, useState } from "react";
 import leftArrow from "../assets/icons/left-arrow.png";
 import rightArrow from "../assets/icons/right-arrow.png";
+import starfishImage from "../assets/images/starfish.jpg";
+import northernLights from "../assets/images/northern-lights.jpg";
+import sedona from "../assets/images/sedona.jpg";
+import green from "../assets/images/green.jpg";
 
 const Carosel = () => {
-  // OTHER METHODS TESTED
-  // const [initialLoad, setInitialLoad] = useState(true);
-  // const [currentImg, setCurrentImg] = useState(1);
-
   // useState METHOD
   const [img1, setImg1] = useState(-60);
   const [img2, setImg2] = useState(0);
   const [img3, setImg3] = useState(60);
   const [img4, setImg4] = useState(120);
 
-  // useReducer METHOD
-  // const initialState = {
-  //   initialLoad: true,
-  //   imgArray: [
-  //     { name: "img1", position: 0, translateX: -60 },
-  //     { name: "img2", position: 1, translateX: 0 },
-  //     { name: "img3", position: 2, translateX: 60 },
-  //     { name: "img4", position: 3, translateX: 120 },
-  //   ],
-  // };
-
-  // const reducer = (state, action) => {
-  //   const copiedArray = [...state.imgArray];
-  //   switch (action.type) {
-  //     case "RIGHT_ARROW_CLICK":
-  //       return {
-  //         initialLoad: false,
-  //         imgArray: copiedArray.map(img => {
-  //           return {
-  //             name: img.name,
-  //             // position: state.position,
-  //             // img.position === 3 ? (img.position = 0) : (img.position += 1),
-  //             translateX: (img.translateX -= 60),
-  //             // img.translateX === -60
-  //             //   ? (img.translateX = 120)
-  //             //   : (img.translateX = img.translateX - 60),
-  //           };
-  //         }),
-  //       };
-  //     case "LEFT_ARROW_CLICK":
-  //       return {
-  //         initialLoad: false,
-  //         imgArray: copiedArray.map(img => {
-  //           return {
-  //             name: img.name,
-  //             // position: state.position,
-  //             // img.position === 0 ? (img.position = 3) : (img.position -= 1),
-  //             translateX: (img.translateX += 60),
-  //             // img.translateX === 120
-  //             //   ? (img.translateX = -60)
-  //             //   : (img.translateX = img.translateX + 60),
-  //           };
-  //         }),
-  //       };
-  //     default:
-  //       throw new Error("Issue updated React useReducer State.");
-  //   }
-  // };
-
-  // const [state, dispatch] = useReducer(reducer, initialState);
-
-  // const rightArrowHandler = () => {
-  //   dispatch({ type: "RIGHT_ARROW_CLICK" });
-  // };
-
-  // const leftArrowHandler = () => {
-  //   dispatch({ type: "LEFT_ARROW_CLICK" });
-  // };
-
-  // console.log(state.imgArray);
-
   // useState METHOD
   const rightArrowHandler = () => {
-    if (img1 === -120) {
-      setImg1(120);
-    } else {
-      setImg1(img1 - 60);
-    }
-    if (img2 === -120) {
-      setImg2(120);
-    } else {
-      setImg2(img2 - 60);
-    }
-    if (img3 === -120) {
-      setImg3(120);
-    } else {
-      setImg3(img3 - 60);
-    }
-    if (img4 === -120) {
-      setImg4(120);
-    } else {
-      setImg4(img4 - 60);
-    }
+    // Garud Clause to prevent slides from moving right any further
+    if (img4 === 0) return;
+
+    setImg1(img1 - 60);
+    setImg2(img2 - 60);
+    setImg3(img3 - 60);
+    setImg4(img4 - 60);
   };
 
   const leftArrowHandler = () => {
-    if (img1 === 120) {
-      setImg1(-120);
-    } else {
-      setImg1(img1 + 60);
-    }
-    if (img2 === 120) {
-      setImg2(-120);
-    } else {
-      setImg2(img2 + 60);
-    }
-    if (img3 === 120) {
-      setImg3(-120);
-    } else {
-      setImg3(img3 + 60);
-    }
-    if (img4 === 120) {
-      setImg4(-120);
-    } else {
-      setImg4(img4 + 60);
-    }
+    // Gaurd clause to prevent slides from moving left any further
+    if (img1 === 0) return;
+
+    setImg1(img1 + 60);
+    setImg2(img2 + 60);
+    setImg3(img3 + 60);
+    setImg4(img4 + 60);
   };
 
-  // const leftArrowHandler = () => {
-  //   setImg1(img1 + 60);
-  //   setImg2(img2 + 60);
-  //   setImg3(img3 + 60);
-  //   setImg4(img4 + 60);
-  // };
+  let trans1 = `translateX(${img1}vh)`;
+  let trans2 = `translateX(${img2}vh)`;
+  let trans3 = `translateX(${img3}vh)`;
+  let trans4 = `translateX(${img4}vh)`;
 
-  console.log(img1, img2, img3, img4);
-
-  // ORIGINAL METHODS TESTED
-  // const [img1, img2, img3, img4] = [0, 1, 2, 3];
-  // const translateAmount;
-
-  // const rightArrowHandler = () => {
-  //   imgArr.forEach(img => {
-  //     if (img === 3) {
-  // 			img = 0;
-  // 			translateAmount = -60;
-  //     } else {
-  // 			img += 1;
-  // 			translateAmount = 60;
-  //     }
-  //     console.log(imgArr);
-  //   });
-  // };
-
-  // const leftArrowHandler = () => {
-  //   imgArr.forEach(img => {
-  //     if (img === 0) {
-  // 			img = 3;
-  // 			translateAmount =
-  //     } else {
-  //       img -= 1;
-  //     }
-  //     console.log(imgArr);
-  //   });
-  // };
+  let css1 = { transform: trans1 };
+  let css2 = { transform: trans2 };
+  let css3 = { transform: trans3 };
+  let css4 = { transform: trans4 };
 
   return (
-    <div className={classes.container}>
-      <img
-        src={leftArrow}
-        onClick={leftArrowHandler}
-        className={classes.leftArrow}
-      />
-      <div className={classes.image1} style={{ translateX: `${img1}vh` }}></div>
-      <div className={classes.image2} style={{ translateX: `${img2}vh` }}></div>
-      <div className={classes.image3} style={{ translateX: `${img3}vh` }}></div>
-      <div className={classes.image4} style={{ translateX: `${img4}vh` }}></div>
-      <img
-        src={rightArrow}
-        onClick={rightArrowHandler}
-        className={classes.rightArrow}
-      />
-    </div>
+    <Fragment>
+      <h2> Paul Munley - Professional Developer</h2>
+      <div className={classes.container}>
+        <img
+          src={leftArrow}
+          onClick={leftArrowHandler}
+          className={classes.leftArrow}
+        />
+        <div className={classes.image1} style={css1}>
+          <img src={green} />
+        </div>
+        <div className={classes.image2} style={css2}>
+          <img src={starfishImage} />
+        </div>
+        <div className={classes.image3} style={css3}>
+          <img src={northernLights} />
+        </div>
+        <div className={classes.image4} style={css4}>
+          <img src={sedona} />
+        </div>
+        <img
+          src={rightArrow}
+          onClick={rightArrowHandler}
+          className={classes.rightArrow}
+        />
+      </div>
+    </Fragment>
   );
 };
 
